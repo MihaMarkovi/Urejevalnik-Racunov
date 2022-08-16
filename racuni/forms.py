5876840e839366ec1989580f7a131f3297db9309
MihaMarkovi <miha0552@gmail.com>
2022-08-16 21:39:28 +0200

    Added history

--- racuni/forms.py
+++ racuni/forms.py
@@ -4,13 +4,17 @@ from .models import Company, Bill
 
 class BillDetailsForm(forms.Form):
     eor = forms.CharField(label="EOR", max_length=100,
-                          widget=forms.TextInput(attrs={'readonly': 'readonly', 'style': 'background-color: darkgray'}))
-    producer = forms.ModelChoiceField(label="Proizvajalec", queryset=Company.objects.all())
-    bill_number = forms.CharField(label='Številka Računa', max_length=200)
-    seller = forms.CharField(label='Prodajalec', max_length=200)
-    tax_level = forms.FloatField(label='Davčna Stopnja')
-    zoi = forms.CharField(label='ZOI', max_length=200)
-    status = forms.BooleanField(label='Potrjeno', required=False)
+                          widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'form-control m-1'}))
+    producer = forms.ModelChoiceField(label="Proizvajalec", queryset=Company.objects.all(),
+                                      widget=forms.Select(attrs={'class': 'form-control m-1'}))
+
+    bill_number = forms.CharField(label='Številka Računa', max_length=200,
+                                  widget=forms.TextInput(attrs={'class': 'form-control m-1'}))
+    seller = forms.CharField(label='Prodajalec', max_length=200,
+                             widget=forms.TextInput(attrs={'class': 'form-control m-1'}))
+    tax_level = forms.FloatField(label='Davčna Stopnja', widget=forms.NumberInput(attrs={'class': 'form-control m-1'}))
+    zoi = forms.CharField(label='ZOI', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control m-1'}))
+    status = forms.BooleanField(label='Potrjeno', required=False)  # widget je CheckboxInput()
 
 
 class ProductDetailsForm(forms.Form):
--- racuni/urls.py
+++ racuni/urls.py
@@ -8,12 +8,15 @@ urlpatterns = [
     path('podjetja/<int:pk>', views.CompanyDetailsView.as_view(), name='company-details'),
     path('podjetja/<int:pk>/izbrisi', views.CompanyDeleteConfirmView.as_view(), name='company-delete'),
     path('podjetja/', views.CompanyView.as_view(), name='companies'),
-    path('podjetja/dodaj', views.AddNewCompany.as_view(), name='add-company'),
+    path('podjetja/dodaj/', views.AddNewCompany.as_view(), name='add-company'),
 
-    path('izpis', views.OutputView.as_view(), name='output'),
+    path('zgodovina/', views.HistoryView.as_view(), name='history'),
+    path('izpis/', views.OutputView.as_view(), name='output'),
 
     path('<str:pk>/', views.BillDetailsView.as_view(), name='bill-details'),
     path('<str:bill_id>/<int:pk>', views.ProductsDetailsView.as_view(), name='product-details'),
     path('<str:bill_id>/<int:pk>/izbrisi', views.ProductDeleteConfirmView.as_view(), name='product-delete'),
-    path('<str:bill_id>/dodaj', views.AddNewProduct.as_view(), name='add-product')
+    path('<str:bill_id>/dodaj', views.AddNewProduct.as_view(), name='add-product'),
+
+
 ]
